e50a974ada39b5ab77cf227ad817f3aaf0a86ebf
Shishir7007 <shishirchhetri0936@gmail.com>
2024-07-14 22:29:45 +0545

    nkjdka

---
+++ assets/JS/min.js
@@ -0,0 +1,29 @@
+// function myFun() {
+//     alert("Welcome to JS");
+// }
+
+// let a=10; //declare//
+// document.write(a);
+
+// function aalu(){
+//     document.write(a);
+//      a=20;  //assign//
+//     document.write(a);
+// }
+// aalu();
+// function lalu(){
+//     const a=30;
+// }
+// lalu();
+
+// alert(4+9);
+// console.log(6+23);
+document.getElementById("demo").innerHTML = "Hello World";
+var x =  document.getElementsByClassName("new");
+document.getElementById("new").innerHTML =
+ x[0].innerHTML + x[1].innerHTML + x[2].innerHTML;
+ document.getElementsByTagName("h1").innerHTML = "Hello World";
+
+ document.querySelector("#demo").innerHTML = "Hello World";
+ document.querySelector(".new").innerHTML = "Hello World";
+ document.querySelectorAll(".new").innerHTML = "Hello World";
